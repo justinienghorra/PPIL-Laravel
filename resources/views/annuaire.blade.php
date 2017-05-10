c640ee5970f6a74316f28a91177fdc119db6c2f3
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-05-10 17:01:59 +0200

    Modif annuaire

--- resources/views/annuaire.blade.php
+++ resources/views/annuaire.blade.php
@@ -72,6 +72,52 @@ Récapitulatif des enseignants
       </div>
         </div>
     </div>
+
+    @include('includes.buttonImportExport')
+
+    <div id="modal_export" class="modal">
+        <div class="modal-content">
+            <h4>Exportation des données</h4>
+            <div class="row">
+                <div class="input-field col s12">
+                    <select >
+                        <option class="blue-text" value="1">PDF</option>
+                        <option class="blue-text"  value="2">CSV</option>
+                        <option class="blue-text" value="3">Excel</option>
+                    </select>
+                    <label>Format du fichier</label>
+                </div>
+            </div>
+
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat blue-text">Exporter</a>
+        </div>
+    </div>
+    <!-- END MODAL EXPORT -->
+
+    <!-- MODAL IMPORT -->
+    <div id="modal_import" class="modal">
+        <div class="modal-content">
+            <h4>Importation d'une liste d'enseignants</h4>
+            
+            <div class="row">
+                <div class="file-field input-field">
+                    <div class="btn btn-flat purple-text">
+                        <span>Fichier</span>
+                        <input type="file">
+                    </div>
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat purple-text">Importer</a>
+        </div>
+    </div>
     
      
 @stop
