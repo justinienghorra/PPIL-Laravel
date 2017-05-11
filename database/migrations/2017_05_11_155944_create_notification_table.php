de9410c4a11d1e87a4f6132be989cb288bf55cda
GZablot <zablot.guillaume@bbox.fr>
2017-05-11 18:11:17 +0200

    add notification migration

---
+++ database/migrations/2017_05_11_155944_create_notification_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('resume')->size(256);
+
+            $table->date('date_notification');
+
+            $table->integer('id_utilisateur_a_notifie')->unsigned();
+            $table->foreign('id_utilisateur_a_notifie')->references('id')->on('users');
+
+            $table->integer('venant_de_id_utilisateur')->unsigned();
+            $table->foreign('venant_de_id_utilisateur')->references('id')->on('users');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification', function (Blueprint $table) {
+            $table->dropForeign(['id_utilisateur_a_notifie', 'venant_de_id_utilisateur']);
+        });
+    }
+}
