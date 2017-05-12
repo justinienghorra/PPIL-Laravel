e9f03b4b32c36f3203f3f450e66a895f887d952a
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-05-12 22:50:43 +0200

    Ajout fichier seeder

---
+++ database/seeds/StatutsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Statuts;
+
+class StatutsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $s = new Statuts;
+        $s->statut = 'ATER';
+        $s->save();
+        $s = new Statuts;
+        $s->statut = 'PRAG';
+        $s->save();
+        $s = new Statuts;
+        $s->statut = 'EC';
+        $s->save();
+        $s = new Statuts;
+        $s->statut = 'Doctorant';
+        $s->save();
+        $s = new Statuts;
+        $s->statut = 'Vacataire';
+        $s->save();
+    }
+}
