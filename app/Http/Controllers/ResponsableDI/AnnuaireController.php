1ae38fef80ee58893dc3704659b254d0412e71a9
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-05-13 22:24:26 +0200

    Avancement dans l'importation de la liste d'enseignant

--- app/Http/Controllers/ResponsableDI/AnnuaireController.php
+++ app/Http/Controllers/ResponsableDI/AnnuaireController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers\ResponsableDI;
 
 use App\Http\Controllers\Controller;
 
+use App\Statut;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Validator;
 use Illuminate\View\View;
 
 class AnnuaireController extends Controller
 {
+
+    protected $civilite_array;
+    protected $statut_array;
+
+    public function __construct()
+    {
+        $this->civilite_array = array("M", "Mme");
+        $this->statut_array = Statut::select('statut')->get();
+    }
+
     /**
      * Retourne la vue de l'annuaire
      *
@@ -49,6 +61,35 @@ class AnnuaireController extends Controller
         //TODO check le type du fichier
         $file = $request->file('file_csv');
 
+        $f = fopen($file->path(), "r");
+
+        $csv_content = fgetcsv($f);
+        while ($csv_content) {
+
+            $validator = Validator::make($request->all(), [
+                '0' => 'required|in_array:civilite_array',
+                '1' => 'required|string|max:255',
+                '2' => 'required|string|max:255',
+                '3' => 'required',
+                '4' => 'required',
+                '5' => 'required|in_array',
+            ]);
+
+            $user = new User;
+            $user->civilite = $csv_content[0];
+            $user->prenom = $csv_content[1];
+            $user->nom = $csv_content[2];
+            $user->email = $csv_content[3];
+            $user->adresse = $csv_content[4];
+            $user->id_statut = Statut::where('statut', $csv_content[5])->first()->id;
+            $user->password = bcrypt("password");
+            $user->attente_validation = false;
+            $user->save();
+            $csv_content = fgetcsv($f);
+        }
+
+
+        fclose($f);
         return redirect('/di/annuaire');
     }
 }
