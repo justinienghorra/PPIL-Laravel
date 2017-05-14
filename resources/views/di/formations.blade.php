f673ba99ceec67f13571fc8276489ef353ac54cd
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-05-14 20:30:29 +0200

    Changement vue formations pour le resp DI

--- resources/views/di/formations.blade.php
+++ resources/views/di/formations.blade.php
@@ -16,7 +16,13 @@
                 <td><a href="/formations/{{$formation->nom}}">{{$formation->nom}}</a></td>
                 <td>{{$formation->description}}</td>
                 <td>{{$formation->responsable->user->prenom . " " . $formation->responsable->user->nom }}</td>
-                <td><button name="{{$formation->id}}">Supprimer</button></td>
+                <td>
+                    <form action="/di/formations/delete" method="post">
+                        {{csrf_field()}}
+                        <input type="hidden" value="{{$formation->id}}">
+                        <button type="submit">Supprimer</button>
+                    </form>
+                </td>
             </tr>
 
         @endforeach
@@ -28,7 +34,7 @@
 <hr>
 
 <h2>Ajouter une formation</h2>
-<form action="/di/formations/add">
+<form method="post" action="/di/formations/add">
     <label for="nom">Nom : </label>
     <input type="text" name="nom">
     <label for="description">Description : </label>
@@ -39,13 +45,18 @@
             <option value="{{$user->email}}">{{ $user->prenom . " " . $user->nom }}</option>
         @endforeach
     </select>
-    <button type="submit">Ok</button>
+    <button type="submit">Ajouter</button>
 </form>
 
 <hr>
 <h2>Importer un CSV</h2>
-
+<form method="post" action="/di/formations/import" enctype="multipart/form-data">
+    {{ csrf_field() }}
+    <input type="file" name="file_csv" />
+    <button type="submit">Envoyer</button>
+</form>
 <hr>
 <h2>Exportation</h2>
+<a href="/di/formations.csv">Exporter</a>
 </body>
 </html>
