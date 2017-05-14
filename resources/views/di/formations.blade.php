ecd9eb975e54d111cc6bbdac8332c733e1b57504
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-05-14 19:59:56 +0200

    Changement vue formations pour le resp DI

--- resources/views/di/formations.blade.php
+++ resources/views/di/formations.blade.php
@@ -13,9 +13,10 @@
         @foreach($formations as $formation)
 
             <tr>
-                <td>{{$formation->nom}}</td>
+                <td><a href="/formations/{{$formation->nom}}">{{$formation->nom}}</a></td>
                 <td>{{$formation->description}}</td>
                 <td>{{$formation->responsable->user->prenom . " " . $formation->responsable->user->nom }}</td>
+                <td><button>Supprimer</button></td>
             </tr>
 
         @endforeach
@@ -24,5 +25,17 @@
 
 @endif
 
+<hr>
+
+<h2>Ajouter une formation</h2>
+<form action="/di/formations/add">
+
+</form>
+
+<hr>
+<h2>Importer un CSV</h2>
+
+<hr>
+<h2>Exportation</h2>
 </body>
 </html>
