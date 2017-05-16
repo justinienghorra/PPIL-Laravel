e4425f702f8038151a04326c7a1fbf0875936440
Justinien <justinienghorra@hotmail.fr>
2017-05-16 09:36:38 +0200

    début branch

--- resources/views/mesUE.blade.php
+++ resources/views/mesUE.blade.php
@@ -18,7 +18,7 @@ Liste de vos UE
                 <h4 class="header light">Description</h4>
                 
                 <!-- Contenu du premier EC -->
-
+<!--
                 <p class="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada lacus risus, at sagittis mi scelerisque vel. </p>
                 
 
@@ -118,7 +118,8 @@ Liste de vos UE
 
                 
                 <!-- Fin du Contenu du premier EC -->
-            </div>
+            <!--
+					</div>
             
           
 
