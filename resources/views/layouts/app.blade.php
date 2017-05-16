328c234127261491e380d69b8d354477db977116
Alexandre Klein <kleinalexandre95@gmail.com>
2017-05-16 09:30:57 +0200

    retour bouton deconnexion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,13 +40,13 @@
                             <li><a href="{{ route('login') }}">Connexion</a></li>
                             <li><a href="{{ route('register') }}">Inscription</a></li>
                         
-                        @else
+                        @else                        
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <ul class="dropdown-menu" role="menu">
+                                <ul class="" role="menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
@@ -74,7 +74,6 @@
 
             </div>
         </nav>
-
         @yield('content')
     </div>
 
