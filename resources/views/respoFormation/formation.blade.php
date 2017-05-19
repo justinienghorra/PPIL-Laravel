3e15eceb49f94bbd9151ee6db5dd859cd62bba84
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-05-19 20:35:54 +0200

    Avancement front respoFormation

--- resources/views/respoFormation/formation.blade.php
+++ resources/views/respoFormation/formation.blade.php
@@ -1,3 +1,12 @@
+<?php
+function redOrGreen($attendu, $affecte)
+{
+    if ($attendu == $affecte) {
+        return '<span class=" green-text">';
+    }
+    return '<span class=" red-text">';
+}
+?>
 @extends('layouts.main')
 @section('title')
     {{ $formation->nom }}
@@ -15,8 +24,136 @@
                 <p><strong>Responsable :</strong> {{$formation->responsable->user->prenom . " " . $formation->responsable->user->nom }}</p>
             </blockquote>
 
-            <table border="1" id="tableau_ues">
+            <ul class="collapsible white" data-collapsible="expandable">
+                <li class="collection-header orange-text">
+                    <h4 class="center">Liste des UE</h4>
+                </li>
+                @foreach($ues as $ue)
+                    <div class="row">
+                        <h4 class="header light">{{$ue['description']}}</h4>
+                        <blockquote>
+                            <h4 class="light">Synthèse</h4>
+                        </blockquote>
+                    </div>
+                    <div class="row">
+                        <table class="bordered">
+                            <thead>
+                            <tr>
+                                <th class="center"></th>
+                                <th class="center">CM</th>
+                                <th class="center">TD</th>
+                                <th class="center">TP</th>
+                                <th class="center">EI</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th class="center">Volume attendu</th>
+                                <td class="center">{{$ue->cm_volume_attendu}}</td>
+                                <td class="center">{{$ue->td_volume_attendu}}</td>
+                                <td class="center">{{$ue->tp_volume_attendu}}</td>
+                                <td class="center">{{$ue->ei_volume_attendu}}</td>
+                            </tr>
+                            <tr>
+                                <th class="center">Volume affecté</th>
+                                <td class="center">
+                                    {!! redOrGreen($ue->cm_volume_attendu, $ue->getCMNbHeuresAffectees()) !!}
+                                    {{$ue->getCMNbHeuresAffectees()}}
+                                </td>
+                                <td class="center">
+                                    {!! redOrGreen($ue->td_volume_attendu, $ue->getTDNbHeuresAffectees()) !!}
+                                    {{$ue->getTDNbHeuresAffectees()}}
+                                </td>
+                                <td class="center">
+                                    {!! redOrGreen($ue->tp_volume_attendu, $ue->getTPNbHeuresAffectees()) !!}
+                                    {{$ue->getTPNbHeuresAffectees()}}
+                                </td>
+                                <td class="center">
+                                    {!! redOrGreen($ue->ei_volume_attendu, $ue->getEINbHeuresAffectees()) !!}
+                                    {{$ue->getEINbHeuresAffectees()}}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th class="center">Nombre de groupes attendus</th>
+                                <td></td>
+                                <td class="center">{{$ue->td_nb_groupes_attendus}}</td>
+                                <td class="center">{{$ue->tp_nb_groupes_attendus}}</td>
+                                <td class="center">{{$ue->ei_nb_groupes_attendus}}</td>
+                            </tr>
+                            <tr>
+                                <th class="center">Nombre de groupes affectés</th>
+                                <td></td>
+                                <td class="center">
+                                    {!! redOrGreen($ue->td_nb_groupes_attendus, $nbGroupesTDAffectes) !!}
+                                    {{$nbGroupesTDAffectes}}
+                                </td>
+                                <td class="center">
+                                    {!! redOrGreen($ue->tp_nb_groupes_attendus, $nbGroupesTPAffectes) !!}
+                                    {{$nbGroupesTPAffectes}}
+                                </td>
+                                <td class="center">
+                                    {!! redOrGreen($ue->ei_nb_groupes_attendus, $nbGroupesEIAffectes) !!}
+                                    {{$nbGroupesEIAffectes}}
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <blockquote class="hide-on-med-and-down"><h4 class="light">Détails par enseignant</h4>
+                        </blockquote>
 
+                        <table class="hide-on-med-and-down bordered">
+                            <thead>
+                            <tr>
+
+                                <th class="center">Nom</th>
+                                <th class="center">CM</th>
+                                <th class="center" colspan="2">TD</th>
+                                <th class="center" colspan="2">TP</th>
+                                <th class="center" colspan="2">EI</th>
+                            </tr>
+                            </thead>
+
+                            <thead>
+                            <tr>
+                                <th></th>
+
+                                <th class="center">Heures</th>
+                                <th class="center">Nombre de groupes</th>
+                                <th class="center">Heures par groupe</th>
+                                <th class="center">Nombre de groupes</th>
+                                <th class="center">Heures par groupe</th>
+                                <th class="center">Nombre de groupes</th>
+                                <th class="center">Heures par groupe</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($enseignants[$ue->id] as $enseignant)
+                                <tr>
+                                    <td class="center">{{$enseignant->nom . " " . $enseignant->prenom}}</td>
+                                    <td class="center">{{$enseignant->cm_nb_heures}}</td>
+                                    <td class="center">{{$enseignant->td_nb_groupes}}</td>
+                                    <td class="center">{{$enseignant->td_heures_par_groupe}}</td>
+                                    <td class="center">{{$enseignant->tp_nb_groupes}}</td>
+                                    <td class="center">{{$enseignant->tp_heures_par_groupe}}</td>
+                                    <td class="center">{{$enseignant->ei_nb_groupes}}</td>
+                                    <td class="center">{{$enseignant->ei_heures_par_groupe}}</td>
+                                </tr>
+                            </tbody>
+                            @endforeach
+                        </table>
+                @endforeach
+            </ul>
+
+
+
+
+
+
+            <table border="1" id="tableau_ues">
 
                 @foreach($ues as $ue)
                     <tr id="{{$ue->id}}">
