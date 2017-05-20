517f9c34fc27f1216f5fcc5d8a2c864c0b9dae1c
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-05-20 22:10:10 +0200

    Ajout seeder respi DI prod

---
+++ database/seeds/CreateRespiDIProduction.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CreateRespiDIProduction extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User;
+        $admin->nom = "Dupont";
+        $admin->prenom = "Jean";
+        $admin->email = "jean.dupont@gmail.com";
+        $admin->password = bcrypt("ppil-l3-resp-di-1664");
+        $admin->adresse = "45 Rue de la Liberté, 54000 Nancy";
+        $admin->civilite = "M.";
+        $admin->attente_validation = false;
+        $admin->id_statut = \App\Statut::where('statut', 'Aucun')->first()->id;
+        $admin->save();
+
+        $respDI = new \App\ResponsableDepInfo;
+        $respDI->id_utilisateur = User::where('email', "jean.dupont@gmail.com")->first()->id;
+        $respDI->save();
+    }
+}
