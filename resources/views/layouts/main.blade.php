6beab11e3f718bd34665d2c4bc67b9de6a7a9d49
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-05-20 14:43:23 +0200

    fix background

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,36 +4,56 @@
     @include('includes.head')
 </head>
 <body>
-    @include('includes.header-main')
-    <main>
+@include('includes.header-main')
+<main>
 
-        <div class="section no-pad-bot" id="index-banner">
+    <div id="main-container" class="section no-pad-bot" id="index-banner">
 
-            <div class="container">
+        <div class="container">
             <br><br>
 
             @yield('content')
 
-            </div>
         </div>
+    </div>
 
-    </main>
+</main>
 
-    @include('includes.footer')
-    <script>
-    $( document ).ready(function(){
-      $('select').material_select();
-      $('.modal').modal();
-      $(".button-collapse").sideNav();
-      $('.collapsible').collapsible();
-      $('.dropdown-button').dropdown({ 
-        hover: true, // Activate on hover
-        belowOrigin: true,
-        constrainWidth: false,
-      });
-      $('.tooltipped').tooltip({delay: 50});
+@include('includes.footer')
+<script>
+
+    function update() {
+        var velocity = 0.5;
+        var pos = $(window).scrollTop();
+        $('.container').each(function () {
+            var $element = $(this);
+            // subtract some from the height b/c of the padding
+            var height = $element.height() - 18;
+            $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px');
+        });
+    };
+
+    $(document).ready(function () {
+
+        $('select').material_select();
+
+        $('.modal').modal();
+
+        $('.button-collapse').sideNav();
+
+        $('.collapsible').collapsible();
+
+        $('.dropdown-button').dropdown({
+            hover: true, // Activate on hover
+            belowOrigin: true,
+            constrainWidth: false,
+        });
+
+        $('.tooltipped').tooltip({delay: 50});
+
+        $(window).bind('scroll', update);
     });
-    
-  </script>
+
+</script>
 </body>
 </html>
