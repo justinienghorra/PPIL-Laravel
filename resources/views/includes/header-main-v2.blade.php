f3a3ae42285054af3497120c8cb2c201093418b7
GZablot <zablot.guillaume@bbox.fr>
2017-05-22 19:16:21 +0200

    fix photo header

--- resources/views/includes/header-main-v2.blade.php
+++ resources/views/includes/header-main-v2.blade.php
@@ -60,7 +60,7 @@
             <li><a class="dropdown-button" href="#!" data-activates="dropdown_user">{{$userA->civilite}}
                     {{$userA->nom}}<i class="material-icons right">arrow_drop_down</i></a></li>
             @if(isset($userA->photo))
-	    <li><img src={{$photoUrl}} class="navbar-pic circle" alt=""></li>
+	    <li><img src="/images{{$photoUrl}}" class="navbar-pic circle" alt=""></li>
             @else
 	      <li><img src="/images/default.jpg" class="navbar-pic circle" alt=""></li>
             @endif
